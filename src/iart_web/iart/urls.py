d7cfee2952b17d2cbaae58206f6f019590ce68dc
matthias springstein <matthias.springstein@tib.eu>
2020-11-24 10:27:14 +0000

    Serve uploaded image if debug mode is enabled.

--- src/iart_web/iart/urls.py
+++ src/iart_web/iart/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path('', include('frontend.urls')), path('admin/', admin.site.urls)]
+urlpatterns = [path("", include("frontend.urls")), path("admin/", admin.site.urls)]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.UPLOAD_URL, document_root=settings.UPLOAD_ROOT)
