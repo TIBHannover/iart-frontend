308beab6c0c58652de54e433e903cc1cf75587f1
sschneider <stefanie.schneider@cereality.net>
2021-07-06 16:31:17 +0200

    Adapt collection modals

--- src/components/ModalCollectionList.vue
+++ src/components/ModalCollectionList.vue
@@ -1,28 +1,46 @@
 <template>
-  <v-dialog v-model="dialog" max-width="900px">
+  <v-dialog
+    v-model="dialog"
+    max-width="900px"
+  >
     <template v-slot:activator="{ on }">
-      <v-btn v-on="on" class="register" text block large>
+      <v-btn
+        v-on="on"
+        class="register"
+        text
+        block
+        large
+      >
         <v-badge
           :content="collections.length"
+          :value="collections.length > 0"
           color="accent"
           inline
-          :value="collections.length > 0"
         >
-          {{ $t("modal.collections.title") }}
+          {{ $t("modal.collection.list.title") }}
         </v-badge>
       </v-btn>
     </template>
     <v-card>
       <v-card-title class="mb-2">
-        {{ $t("modal.collections.title") }}
+        {{ $t("modal.collection.list.title") }}
 
-        <v-btn icon @click.native="dialog = false" absolute top right>
+        <v-btn
+          icon
+          @click.native="dialog = false"
+          absolute
+          top
+          right
+        >
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-card-title>
 
       <v-card-text>
-        <v-card v-for="collection in collections" :key="collection.name">
+        <v-card
+          v-for="collection in data"
+          :key="collection.id"
+        >
           <v-card-title>{{ collection.name }}</v-card-title>
           <v-card-text>
             <p>{{ $t("modal.collections.count") }}: {{ collection.count }}</p>
@@ -32,17 +50,24 @@
             </p>
           </v-card-text>
           <v-card-actions>
-            <v-btn color="grey" width="50%" plain @click="dialog = false">
+            <v-btn
+              color="grey"
+              width="50%"
+              plain
+              @click="dialog = false"
+            >
               {{ $t("button.show") }}
             </v-btn>
-            <v-btn color="grey" width="50%" plain @click="dialog = false">
+            <v-btn
+              color="grey"
+              width="50%"
+              plain
+              @click="dialog = false"
+            >
               {{ $t("button.delete") }}
             </v-btn>
           </v-card-actions>
         </v-card>
-        <v-btn color="grey" width="100%" plain @click="dialog = false">
-          {{ $t("button.cancel") }}
-        </v-btn>
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -50,15 +75,17 @@
 
 <script>
 import { keyInObj } from "../plugins/helpers";
-
 export default {
-  props: ["collections", "show-badges"],
   data() {
     return {
       dialog: false,
     };
   },
-  computed: {},
+  computed: {
+    data() {
+      return this.$store.state.collection.collections;
+    },
+  },
   methods: {},
   watch: {
     dialog(value) {
--- src/components/ModalCollectionUpload.vue
+++ src/components/ModalCollectionUpload.vue
@@ -1,15 +1,30 @@
 <template>
-  <v-dialog v-model="dialog" max-width="900px">
+  <v-dialog
+    v-model="dialog"
+    max-width="450px"
+  >
     <template v-slot:activator="{ on }">
-      <v-btn v-on="on" class="register" text block large>
-        {{ $t("modal.collection.title") }}
+      <v-btn
+        v-on="on"
+        class="register"
+        text
+        block
+        large
+      >
+        {{ $t("modal.collection.upload.title") }}
       </v-btn>
     </template>
     <v-card>
       <v-card-title class="mb-2">
-        {{ $t("modal.collection.title") }}
+        {{ $t("modal.collection.upload.title") }}
 
-        <v-btn icon @click.native="dialog = false" absolute top right>
+        <v-btn
+          @click.native="dialog = false"
+          absolute
+          top
+          right
+          icon
+        >
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-card-title>
@@ -19,7 +34,7 @@
 
         <v-text-field
           v-model="collection.name"
-          :placeholder="$t('modal.collection.name')"
+          :placeholder="$t('modal.collection.upload.name')"
           counter="25"
           :rules="[checkLength]"
           clearable
@@ -28,8 +43,8 @@
         <v-file-input
           v-model="collection.image"
           accept="image/png, image/jpeg, image/gif, application/zip,application/x-tar, application/x-bzip2, application/gzip, application/x-xz, application/x-gtar"
-          :placeholder="$t('modal.collection.imagefile.label')"
-          prepend-icon="mdi-camera"
+          :placeholder="$t('modal.collection.upload.imagefile.label')"
+          prepend-icon="mdi-image-outline"
           :rules="[checkImageFile]"
           show-size
         ></v-file-input>
@@ -37,29 +52,33 @@
         <v-file-input
           v-model="collection.meta"
           accept="text/csv, application/json, application/jsonl"
-          :placeholder="$t('modal.collection.metafile.label')"
-          prepend-icon="mdi-file-document"
+          :placeholder="$t('modal.collection.upload.metafile.label')"
+          prepend-icon="mdi-text-box-outline"
           :rules="[checkMetaFile]"
           show-size
           :error="metaErrorMessage"
           :error-messages="metaErrorMessage"
         >
         </v-file-input>
-
-        <v-btn color="primary" width="50%" plain @click="upload">
+      </v-card-text>
+      <v-card-actions class="px-6 pb-6">
+        <v-btn
+          :disabled="disabled"
+          @click="upload"
+          color="accent"
+          block
+          rounded
+          depressed
+        >
           {{ $t("button.upload") }}
         </v-btn>
-        <v-btn color="grey" width="50%" plain @click="dialog = false">
-          {{ $t("button.cancel") }}
-        </v-btn>
-      </v-card-text>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 import { keyInObj } from "../plugins/helpers";
-
 export default {
   data() {
     return {
@@ -68,6 +87,18 @@ export default {
     };
   },
   computed: {
+    disabled() {
+      if (this.collection && Object.keys(this.collection).length) {
+        if (
+          this.collection.name &&
+          this.checkImageFile() === true &&
+          this.checkMetaFile() === true
+        ) {
+          return false;
+        }
+      }
+      return true;
+    },
     metaErrorMessage() {
       const uploadError = this.$store.state.collection.uploadError;
       if (!uploadError) {
@@ -78,9 +109,9 @@ export default {
       }
       if (uploadError.type === "unknown_fields") {
         console.log("Match");
-        return this.$t("modal.collection.metafile.unknown_fields", {
-          field_names:
-            this.$store.state.collection.uploadError.unknown_fields.join(", "),
+        return this.$t("modal.collection.upload.metafile.unknown", {
+          field_names: this.$store.state.collection.uploadError.unknown_fields
+            .join(", "),
         });
       }
       return null;
@@ -89,32 +120,26 @@ export default {
   methods: {
     upload() {
       this.$store.dispatch("collection/upload", this.collection);
-      //   this.dialog = false;
+      this.dialog = false;
     },
     checkImageFile() {
       const value = this.collection.image;
-
       if (value) {
         if (value.size < 200000000) {
           return true;
         }
-
         return this.$t("modal.search.file.rule");
       }
-
       return this.$t("field.required");
     },
     checkMetaFile() {
       const value = this.collection.meta;
-
       if (value) {
         if (value.size < 20000000) {
           return true;
         }
-
         return this.$t("modal.search.file.rule");
       }
-
       return this.$t("field.required");
     },
     checkLength(value) {
@@ -122,14 +147,11 @@ export default {
         if (value.length < 5) {
           return this.$t("user.register.rules.min");
         }
-
         if (value.length > 25) {
           return this.$t("user.register.rules.max");
         }
-
         return true;
       }
-
       return this.$t("field.required");
     },
   },
