19e60279c1e48ec7a46fefae89cf1778e76f469d
sschneider <stefanie.schneider@cereality.net>
2021-07-06 10:49:35 +0200

    Visual indicator for changed global weights

--- src/components/ModalWeights.vue
+++ src/components/ModalWeights.vue
@@ -21,7 +21,19 @@
         >
           mdi-compass-outline
         </v-icon>
-        <span style="font-size: 14px">
+        <v-badge
+          v-if="changed"
+          color="accent"
+          dot
+        >
+          <span style="font-size: 14px">
+            {{ $t("modal.weights.title") }}
+          </span>
+        </v-badge>
+        <span
+          v-else
+          style="font-size: 14px"
+        >
           {{ $t("modal.weights.title") }}
         </span>
       </v-btn>
@@ -38,6 +50,7 @@
 </template>
 
 <script>
+import { isEqual } from "@/plugins/helpers";
 import Weights from "@/components/Weights.vue";
 export default {
   data() {
@@ -54,6 +67,15 @@ export default {
   computed: {
     toggle() {
       return this.$store.state.user.modal.weights;
+    },
+    changed() {
+      if (this.values && Object.keys(this.values).length) {
+        const defaultValue = { clip_embedding_feature: 1 };
+        if (!isEqual(this.values, defaultValue)) {
+          return true;
+        }
+      }
+      return false;
     },
   },
   watch: {
